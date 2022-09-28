c6962592b5cbb1a30f39cda363f3063297628273
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-09-28 05:25:48 -0700

    A C program that prints exactly a sentence we wante with 'puts'

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *A C program that prints exactly a sentence we wante
+ *avec 'Puts'
+ *Return: 0 for success
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
