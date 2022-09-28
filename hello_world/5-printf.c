246d795a9e941bcfe7bdf92add5791047166d47c
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-09-28 05:38:40 -0700

    a C program that prints exactly the sentence we wante, with 'printf'

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A C program that prints exactly sentence we wante with 'printf'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
