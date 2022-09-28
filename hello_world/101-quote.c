62a2fcbc4fbd5dbe825982fffde9c4ee7888ad28
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-09-28 10:02:44 -0700

    a C program that prints exactly sentece without 'printf' or 'puts'

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A C program that prints exactly a sentence without 'printf' or 'puts'
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
