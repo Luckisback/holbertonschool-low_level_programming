3e0e32ede0b5e2dc2d22e7fa40272b60fd01813e
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-05 06:56:14 -0700

    A function that prints the numbers, from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
