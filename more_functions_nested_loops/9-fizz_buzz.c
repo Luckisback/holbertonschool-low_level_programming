4d52ddf37bca298b62a115cc578aab337e89bffc
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-06 02:24:39 -0700

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Fuction that permit to overcome the challenge
+ *Return: 0 for (Success)
+ **/
+int main(void)
+{
+	int i;
+
+	i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			if (i < 100)
+				printf(" ");
+		}
+
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+		if (i < 100)
+			printf(" ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz");
+		if (i < 100)
+			printf(" ");
+	}
+	else
+	{
+		printf("%d", i);
+		if (i < 100)
+			if (i < 100)
+				printf(" ");
+	}
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
