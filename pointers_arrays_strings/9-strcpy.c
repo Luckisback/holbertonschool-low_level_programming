4562555fd575cc865ebb49e62c720d589c7db47f
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-11 02:52:15 -0700

    A function that copies the string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string into another string
+ * @dest: first argument
+ * @src: second argument
+ * Return: return the expected result
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	char *i = dest;
+	int z = '\0';
+
+	while (*src)
+	{
+		*dest++ = *src++;
+		*dest = z;
+	}
+	return (i);
+}
