054fc61b1f63fdd16387376e927dce99f04a6576
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-17 06:30:08 -0700

    a function that capitalizes all words of a string.

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,24 +10,25 @@ char *cap_string(char *phr)
 {
 	int i = 0;
 
-	while (i)
+	while (phr[i])
 	{
 		while (!(phr[i] >= 'a' && phr[i] <= 'z'))
 			i++;
 
-		if (phr[i - 1] == ';'
-			|| phr[i - 1] == '"'
-			|| phr[i - 1] == '(' 
-			|| phr[i - 1] == ')'
-			|| phr[i - 1] == '{'
-			|| phr[i - 1] == '}'
-			|| phr[i - 1] == '\t'
-			|| phr[i - 1] == '\n'
-			|| phr[i - 1] == '?'
-			|| phr[i - 1] == '!'
-			|| phr[i - 1] == '.'
-			|| phr[i - 1] == ' '
-			|| phr[i - 1] == ',' || i == 0)
+		if (phr[i - 1] == ' ' ||
+				phr[i - 1] == '\t' ||
+				phr[i - 1] == '\n' ||
+				phr[i - 1] == ',' ||
+				phr[i - 1] == ';' ||
+				phr[i - 1] == '.' ||
+				phr[i - 1] == '!' ||
+				phr[i - 1] == '?' ||
+				phr[i - 1] == '"' ||
+				phr[i - 1] == '(' ||
+				phr[i - 1] == ')' ||
+				phr[i - 1] == '{' ||
+				phr[i - 1] == '}' ||
+				i == 0)
 			phr[i] -= 32;
 		i++;
 	}
