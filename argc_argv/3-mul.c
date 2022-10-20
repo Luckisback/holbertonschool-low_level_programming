b1a4b6759add37dfc64ed69a12e906f9909ffcc3
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-20 05:07:22 -0700

    A program that multiplies two numbers.

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,9 +16,12 @@ int main(int argc, char *argv[])
 	if (argc < 1 || argc > 3)
 	{
 		printf("Error");
-		return (1);}
-	res = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", res);
+		return (1);
+	}
+	else
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+	}
 	return (0);
 }
-
