4a5984e4131caada519de1ad5e3be2dff10d1a25
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-24 00:10:24 -0700

    A function that creates an array of chars

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *Myarray = malloc((sizeof(unsigned int) * size) + 1);
-	unsigned int i;	
+	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
