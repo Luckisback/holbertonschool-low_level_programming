6e6d110010a1ee1dffc254563ea343cbbe2debea
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-26 05:48:30 -0700

    A function that allocates memory using malloc.

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -14,15 +14,6 @@ void *malloc_checked(unsigned int b)
 	ptr_b = malloc(sizeof(char *) * b);
 		if (ptr_b == NULL)
 			exit(98);
-	ptr_b = malloc(sizeof(int *) * b);
-			if (ptr_b == NULL)
-				exit(98);
-	ptr_b = malloc(sizeof(float *) * b);
-			if (ptr_b == NULL)
-				exit(98);
-	ptr_b = malloc(sizeof(double *) * b);
-		if (ptr_b == NULL)
-			exit(98);
 	return (ptr_b);
 
 }
