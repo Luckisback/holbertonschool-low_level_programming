b02c2ffed3299a84da518597c0aaa92afd0f1675
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-10-31 03:01:38 -0700

    a new type struct dog with 3 elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG__
+#define __DOG__
+#include <stdio.h>
+/**
+ * struct dog - A structure 
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Structure Dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
