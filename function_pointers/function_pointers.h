cb9ad72477c6618050c33c7d9576504c3e28f4d4
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-01 07:30:20 -0700

    a function that prints a name.

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef __POINTERS__
+#define __POINTERS__
+#include <stdio.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
