d7ba3145db060d21264f2267690a45a216c63b16
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-06 02:32:37 -0800

    a function that prints anything.

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,90 @@
+#include "variadic_functions.h"
+
+/**
+ * pr_char - imprimer un char
+ * @len: argument
+*/
+
+void pr_char(va_list len)
+{
+	char my_var = va_arg(len, int);
+		printf("%c", my_var);
+		printf(", ");
+}
+
+/**
+ * pr_int - imprimer un entier
+ * @len: argument
+*/
+void pr_int(va_list len)
+{
+	int myint = va_arg(len, int);
+	printf("%d", myint);
+	printf(", ");
+}
+
+/**
+ * pr_float - imprimer un float
+ * @len: argument
+*/
+void pr_float(va_list len)
+{
+	double my_fl = va_arg(len, double);
+	printf("%f", my_fl);
+	printf(", ");
+}
+
+/**
+ * pr_string - imprimer un string
+ * @len: argument
+ */
+void pr_string(va_list len)
+{
+	char *str;
+
+	str = va_arg(len, char*);
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s", str);
+	printf(", ");
+}
+
+/**
+ * print_all - a function that prints anything.
+ * @format: last argument named (equivalent of var's type)
+ * @...: argument number
+ **/
+void print_all(const char * const format, ...)
+{
+		op ops[] = {
+		{"c", pr_char},
+		{"i", pr_int},
+		{"f", pr_float},
+		{"s", pr_string},
+		{NULL, NULL}
+		};
+
+	va_list len;
+	int i = 0;
+	int j;
+
+	va_start(len, format);
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (ops[j].typeV)
+		{
+			if (*ops[j].typeV == format[i])
+			{
+				ops[j].f(len);
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(len);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,8 +3,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct op - main var for print_all
+ * @typeV: member
+ * @f: second member function
+ **/
+typedef struct op
+{
+	char *typeV;
+	void (*f)(va_list len);
+
+} op;
 #endif
