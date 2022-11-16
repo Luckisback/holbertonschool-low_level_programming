2672dd025d9a542d73db3d8a45fec62d4cd591c8
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-16 05:47:31 -0800

    a function that adds a new node at the end of a list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,20 +4,20 @@
  * add_node_end - a function that adds a new node at the end of a list
  * @str: data argument
  * @head: head of the list
- * Return: 
+ * Return: the address of the new last node
  **/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *NewEl, *Swap;
-        char *New_str;
-        int New_lgr = 0;
+	list_t *NewEl;
+	list_t *tmp;
+	char *New_str;
+	int New_lgr = 0;
 
-
-        /* allocation de l'espace memoire au nouvel élément */
+	/* allocation de l'espace memoire au nouvel élément */
 	NewEl = malloc(sizeof(list_t));
 
-	if (!NewEl)
+	if (NewEl == NULL)
 	{
 		free(NewEl);
 		return (NULL);
@@ -25,32 +25,28 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	New_str = strdup(str);
 
-        if (New_str == NULL)
-        {
-                free(NewEl);
-                return (NULL);
-        }
+	if (New_str == NULL)
+	{
+		free(NewEl);
+		return (NULL);
+	}
 
-        while (str[New_lgr])
-                New_lgr++;
+	while (str[New_lgr])
+		New_lgr++;
 
-        NewEl->str = New_str;
-        NewEl->len = New_lgr;
-        NewEl->next = NULL;
+	NewEl->str = New_str;
+	NewEl->len = New_lgr;
+	NewEl->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = NewEl;
-	}
 	else
 	{
-		Swap = *head;
-		while (Swap->next != NULL)
-			Swap = Swap->next;
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = NewEl;
 	}
-
-        Swap->next = NewEl;
-
-        return (NewEl);
+	return (*head);
 }
 
