a9e2daf59283d0be5625b127c78af2eff37790c6
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-18 04:21:58 -0800

    a function that adds a new node at the beginning of a list

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - 
+ * add_nodeint - a function that adds a new node at the beginning of a list 
  * @head: the head of the list
  * @n:the argument  
  * Return: the address of the new node
