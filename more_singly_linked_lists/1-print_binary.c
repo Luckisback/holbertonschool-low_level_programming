313d39e591540103559356f695214f9139424e94
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-18 03:43:32 -0800

    a function that returns the number of elements in a linked listint_t list.

---
+++ more_singly_linked_lists/1-print_binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_binary - a function that prints the binary representation
+ * @n: Argument
+ **/
+
+void print_binary(unsigned long int n)
+{
+	int Bin, x, y;
+	int L = 0;
+
+	while (n >= 0)
+	{
+		if ( n == 0 && (x == 0))
+			exit(-1);
+		y = n >> 1;
+		Bin = n -(y * 2);
+		n = y;
+		L++;
+		printf("%d ", Bin);
+	}
+}
