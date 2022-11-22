b5a0f52bcdfe0a9ff87a428eed4a5f42e4bb9aba
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-22 08:04:56 -0800

    a function that appends text at the end of a file.

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,18 +13,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	int nom;
 
 	if (filename == NULL)
-		return (-1)
+		return (-1);
 
 	nom = open(filename, O_WRONLY);
 	if (nom == -1)
 		return (-1);
 	if (text_content == NULL)
 		return (-1);
-	for (text_c = 0; text_content[nbr_l]; nbr_l++)
+	for (nbr_l = 0; text_content[nbr_l]; nbr_l++)
 		;
-	ecrire = write(nom, text_content, nbr_l)
+	ecrire = write(nom, text_content, nbr_l);
+
 	if (ecrire == -1)
 		return (-1);
-	clode(nom);
+	close(nom);
 	return (1);
 }
