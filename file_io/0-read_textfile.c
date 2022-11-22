8f79c0d0c08bdc3f3dc8780e9a966a15afb6ca42
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-22 01:50:20 -0800

    a function that reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints
+ * @filename: the name of the file to be read
+ * @lettre: a structure
+ *
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t ouvrir, lire, ecrire;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return(0);
+
+	ouvrir = open(filename, O_RDONLY);
+
+	lire = read(ouvrir, buffer, letters);
+
+	ecrire = write(STDOUT_FILENO, buffer, lire);
+
+	if (ouvrir == (-1) || lire == (-1) || ecrire != lire)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	close(ouvrir);
+
+	return (lire);
+}
