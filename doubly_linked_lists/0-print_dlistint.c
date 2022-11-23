9b35c674d1be11fb62041525db14b177c24648d3
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-11-23 01:35:06 -0800

    a function that prints all the elements of a dlistint_t list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - a function that prints all the elements of a dlist
  * @h: theargument heaud of the list
- * Return the node's number
+ * Return: the node's number
  **/
 
 size_t print_dlistint(const dlistint_t *h)
